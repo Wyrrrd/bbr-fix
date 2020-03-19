e568e7fa69b6ccabcaf2eecb80e6eb1560f4d84d
Wyrrrd <wyrrrd@gmx.de>
2020-03-19 23:45:21 +0100

    Incremented rail count

--- data-updates.lua
+++ data-updates.lua
@@ -1,9 +1,9 @@
 if settings.startup["bbr_fix_expensive"].value then
-	data.raw["recipe"]["bbr-rail-wood"].ingredients = {{"rail",1},{"wood",5}}
-	data.raw["recipe"]["bbr-rail-iron"].ingredients = {{"rail",1},{"iron-plate",5}}
-	data.raw["recipe"]["bbr-rail-brick"].ingredients = {{"rail",1},{"stone-brick",5}}
+	data.raw["recipe"]["bbr-rail-wood"].ingredients = {{"rail",2},{"wood",5}}
+	data.raw["recipe"]["bbr-rail-iron"].ingredients = {{"rail",2},{"iron-plate",5}}
+	data.raw["recipe"]["bbr-rail-brick"].ingredients = {{"rail",2},{"stone-brick",5}}
 else
-	data.raw["recipe"]["bbr-rail-wood"].ingredients = {{"rail",1},{"wood",1}}
-	data.raw["recipe"]["bbr-rail-iron"].ingredients = {{"rail",1},{"iron-plate",1}}
-	data.raw["recipe"]["bbr-rail-brick"].ingredients = {{"rail",1},{"stone-brick",1}}
+	data.raw["recipe"]["bbr-rail-wood"].ingredients = {{"rail",2},{"wood",1}}
+	data.raw["recipe"]["bbr-rail-iron"].ingredients = {{"rail",2},{"iron-plate",1}}
+	data.raw["recipe"]["bbr-rail-brick"].ingredients = {{"rail",2},{"stone-brick",1}}
 end
